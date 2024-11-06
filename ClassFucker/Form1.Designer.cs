d45f1b8ce58b515fc040bcad0988ac827a693e9c
HighTech <HighTech@H501-05>
2024-11-06 13:55:35 +0900

    시간 증가

--- ClassFucker/Form1.Designer.cs
+++ ClassFucker/Form1.Designer.cs
@@ -273,6 +273,7 @@
             // timer1
             // 
             timer1.Enabled = true;
+            timer1.Interval = 800;
             timer1.Tick += timer1_Tick;
             // 
             // Form1
